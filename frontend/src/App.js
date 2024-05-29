97decacd3dbc02191694f8ab7ff51f5d95028b07
GalaP2001 <pajovicgala07@gmail.com>
2024-05-29 22:42:53 +0200

    kreirana komnponenta za registraciju

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import HomePage from './Pocetna/HomePage';
 import Login from './Autorizacija/Login';
+import Register from './Autorizacija/Register';
 
 function App() {
   return (
     <div className="App">
+      <Register></Register>
         <Login />
       <HomePage></HomePage>
     </div>
