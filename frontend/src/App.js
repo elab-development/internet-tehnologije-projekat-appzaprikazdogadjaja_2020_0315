25a4083b6617c11160768cbafc2570be50e73ea1
GalaP2001 <pajovicgala07@gmail.com>
2024-05-29 22:40:20 +0200

    kreirana komponenta login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import HomePage from './Pocetna/HomePage';
+import Login from './Autorizacija/Login';
 
 function App() {
   return (
     <div className="App">
+        <Login />
       <HomePage></HomePage>
     </div>
   );
