03c2734c5babace03bab868a8866c61429262621
Sergej2001 <pajovicsergej07@gmail.com>
2024-06-24 00:51:21 +0200

    toggle view funkcionalnost

--- frontend/src/Autorizacija/Login.jsx
+++ frontend/src/Autorizacija/Login.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 import './Login.css';
 
 const Login = ({ onLogin }) => {
-    const [email, setEmail] = useState('dschaden@example.com');
+    const [email, setEmail] = useState('stacy71@example.org');
     const [password, setPassword] = useState('password');
     const [error, setError] = useState('');
     const navigate= useNavigate();
